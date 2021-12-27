a8005e09d07dd002b9d5a17e37d11b9ee00b7bfd
ikhsan0000 <ikhsanfirdauz000@gmail.com>
2021-12-27 14:27:56 +0700

    done until 03 Data Fetching

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,34 @@
-import Head from 'next/head'
-import Layout from '../components/layout'
+import EventItem from '@/components/EventItem';
+import Layout from '@/components/layout'
+import Link from 'next/link'
+import { API_URL } from '@/config/index'
 
-
-export default function Home() {
+export default function Home({events}) {
   return (
+
     <Layout>
-      <h1>Dj Events</h1>
+      <h1>Upcoming</h1>
+      {events.length === 0 && <h3>it's lonely here, there's currently no events</h3>}
+      {events && events.map( (e) => (
+        <EventItem key={e.id} evt={e}/>
+      ))}
+      {events && (
+        <Link href={`/events`}>
+          <a className="btn-secondary">
+            See all
+          </a>
+        </Link>
+      )}
+
     </Layout>
   )
+}
+
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+  return {
+    props: { events }
+  }
 }
